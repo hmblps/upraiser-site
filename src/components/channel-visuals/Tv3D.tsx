ff50208289fdd74b8a482546cc50a8a6be80f55d
hmblps <alex@upraiser.co.uk>
2026-08-27 15:48:53 +0300

    fix: reduce scale slightly and shift left to prevent tv clipping on right edge

--- src/components/channel-visuals/Tv3D.tsx
+++ src/components/channel-visuals/Tv3D.tsx
@@ -64,11 +64,11 @@ function TvMesh({
   return (
     <group ref={group}>
       <Center>
-        <group rotation={[0.15, 0, 0]}>
-          <group rotation={[0, -Math.PI / 2, 0]} scale={2.9} position={[0, -0.7, -0.5]}>
+        <group rotation={[0.15, 0, 0]} position={[-0.2, 0, 0]}>
+          <group rotation={[0, -Math.PI / 2, 0]} scale={2.6} position={[0, -0.7, -0.5]}>
             <TvModel mode={mode} />
           </group>
-          <group rotation={[Math.PI / 2, 0, 0]} scale={5.8} position={[1.2, 0.6, 1.0]}>
+          <group rotation={[Math.PI / 2, 0, 0]} scale={5.2} position={[1.2, 0.6, 1.0]}>
             <TabletModel mode={mode} />
           </group>
         </group>
