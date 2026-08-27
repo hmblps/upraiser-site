9033de51bbd1cb28c7a1251eefcd21e32e6f618a
hmblps <alex@upraiser.co.uk>
2026-08-27 15:50:43 +0300

    fix: flip ipad 180 degrees to correct orientation

--- src/components/channel-visuals/Tv3D.tsx
+++ src/components/channel-visuals/Tv3D.tsx
@@ -68,7 +68,7 @@ function TvMesh({
           <group rotation={[0, -Math.PI / 2, 0]} scale={2.6} position={[0, -0.7, -0.5]}>
             <TvModel mode={mode} />
           </group>
-          <group rotation={[Math.PI / 2, 0, 0]} scale={5.2} position={[1.2, 0.6, 1.0]}>
+          <group rotation={[Math.PI / 2, 0, Math.PI]} scale={5.2} position={[1.2, 0.6, 1.0]}>
             <TabletModel mode={mode} />
           </group>
         </group>
