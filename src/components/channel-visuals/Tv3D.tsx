392111a65373cdd20c002ab4de04adaa1336b076
hmblps <alex@upraiser.co.uk>
2026-08-27 16:14:58 +0300

    fix: move tablet closer to tv on x and z axes

--- src/components/channel-visuals/Tv3D.tsx
+++ src/components/channel-visuals/Tv3D.tsx
@@ -68,7 +68,7 @@ function TvMesh({
           <group rotation={[0, -Math.PI / 2, 0]} scale={2.6} position={[0, -0.7, -0.5]}>
             <TvModel mode={mode} />
           </group>
-          <group rotation={[Math.PI / 2, 0, 0]} scale={5.2} position={[1.2, 0.6, 1.0]}>
+          <group rotation={[Math.PI / 2, 0, 0]} scale={5.2} position={[0.9, 0.6, 0.5]}>
             <TabletModel mode={mode} />
           </group>
         </group>
