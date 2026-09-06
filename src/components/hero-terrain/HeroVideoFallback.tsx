c2f84a7c4a658f123d7822417ad8c8aa5c5b9b05
hmblps <alex@upraiser.co.uk>
2026-09-06 11:31:04 +0300

    fix(lint): remove unused useState

--- src/components/hero-terrain/HeroVideoFallback.tsx
+++ src/components/hero-terrain/HeroVideoFallback.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useScroll } from "../../context/ScrollContext";
 import { useTheme } from "../../context/ThemeContext";
 
